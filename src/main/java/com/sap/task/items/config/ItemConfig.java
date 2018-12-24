5abb6b0b5f2440569079f276007aac179202f8a6
Mohsin Khan <this.mohsin@gmail.com>
2018-12-23 20:48:15 -0800

    adding javadoc

--- src/main/java/com/sap/task/items/config/ItemConfig.java
+++ src/main/java/com/sap/task/items/config/ItemConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+import com.sap.task.items.manager.CacheManager;
+
 
 /**
  * Register, inject or override configuration in springframework, required for application.
@@ -14,10 +16,16 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 public class ItemConfig
 {
 
+	/**
+	 * Configuring threads pool executor for applicatiion.
+	 * {@link CacheManager} will use this thread pool executor to spawn cache cleanup task.
+	 *
+	 * @return thread pool executor
+	 */
 	@Bean()
 	public ThreadPoolTaskScheduler taskScheduler()
 	{
-		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+		final ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
 		taskScheduler.setPoolSize(2);
 		taskScheduler.setThreadNamePrefix("Async-");
 		return taskScheduler;
